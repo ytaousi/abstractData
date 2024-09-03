3b090f519bb8f57aaf26af966e79969e11b1f937
ytaousi <yassir.taous@gmail.com>
2024-09-02 22:10:38 -0400

    .. iterator

--- includes/iterator.hpp
+++ includes/iterator.hpp
@@ -2,9 +2,20 @@
 #define ITERATOR_HPP
 
 #include "iterator_traits.hpp"
-
+#include <memory>
 
 namespace ft {
+template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
+class iterator
+{
+    public:
+        typedef T value_type;
+        typedef Distance difference_type;
+        typedef Pointer pointer;
+        typedef Reference reference;
+        typedef Category iterator_category; 
+};
+
 
 }
 
